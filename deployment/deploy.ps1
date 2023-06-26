c7e5916952fc9655afb1c02d9cb4dc33b27dbeca
Matt DeKrey <mattdekrey@gmail.com>
2023-06-26 09:49:32 -0500

    Change deploy to redeploy same image tag

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -11,6 +11,7 @@ az acr login -n 'principlestudios'
 docker push "$registryLoginServer/$($imageName):$imageTag"
 
 # kubectl create configmap -n img-convert env-vars --from-env-file=.env
-helm upgrade --install -n $k8sNamespace $releaseName --create-namespace --repo https://principlestudios.github.io/helm-charts single-container --set-string "image.repository=$registryLoginServer/$imageName,image.tag=$imageTag" --values ./deployment/values.yaml --values ./deployment/values.prod.yaml
+# helm upgrade --install -n $k8sNamespace $releaseName --create-namespace --repo https://principlestudios.github.io/helm-charts single-container --set-string "image.repository=$registryLoginServer/$imageName,image.tag=$imageTag" --values ./deployment/values.yaml --values ./deployment/values.prod.yaml
+kubectl -n $k8sNamespace rollout restart deployment $releaseName-single-container
 
 Pop-Location
